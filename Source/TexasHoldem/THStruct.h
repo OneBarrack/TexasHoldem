b3980e3c6f5d23a6b00a78804ed7d98d914130e0
OneBarrack <wnsdd1@gmail.com>
2022-08-15 01:51:26 +0900

    * Enum class 및 Struct class 추가

---
+++ Source/TexasHoldem/THStruct.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "THStruct.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEXASHOLDEM_API UTHStruct : public UObject
+{
+	GENERATED_BODY()
+	
+};
